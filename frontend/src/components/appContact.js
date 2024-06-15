0163034efd85b0b40fb69566df04f022218dd7a0
abheist <abhishek291093@gmail.com>
2024-06-15 13:46:41 -0300

    working project

--- frontend/src/components/appContact.js
+++ frontend/src/components/appContact.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const AppContact = ({helloAbhishek}) => {
+    const [state, setState] = useState(() => 'Hey Abhishek!');
+
     return (
         <div>
             <h1>Hello, Contact Page!</h1>
-            <h2>{helloAbhishek}</h2>
+            <div>
+                <p><b>Variable from Django:</b> {helloAbhishek}</p>
+                <p><b>Variable from React:</b> {state}</p>
+            </div>
         </div>
     );
 };
